084e95b640eb69022d5fbc8ceaf619ead2bcdeec
abdul <abdul.tanveer@gmail.com>
2020-02-06 11:57:43 +0530

    geocoder intent service url

--- app/src/main/java/com/spot/actday4/MyService.java
+++ app/src/main/java/com/spot/actday4/MyService.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.widget.Toast;
 
+//https://github.com/android/location-samples/blob/master/LocationAddress/app/src/main/java/com/google/android/gms/location/sample/locationaddress/FetchAddressIntentService.java
+
 public class MyService extends Service {
     public MyService() {
     }
@@ -27,7 +29,7 @@ public class MyService extends Service {
          super.onStartCommand(intent, flags, startId);
          String data = intent.getExtras().getString("actkey");
         Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
-
+       // stopSelf();
         return START_STICKY;
     }
 
