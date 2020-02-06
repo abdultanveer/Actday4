7046780c50e6ac704ab6bed671a2bd4578116854
abdul <abdul.tanveer@gmail.com>
2020-02-06 17:10:19 +0530

    headline itemclicklistener

--- app/src/main/java/com/spot/actday4/HeadlinesFragment.java
+++ app/src/main/java/com/spot/actday4/HeadlinesFragment.java
@@ -3,29 +3,46 @@ package com.spot.actday4;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HeadlinesFragment extends Fragment {
+public class HeadlinesFragment extends Fragment implements AdapterView.OnItemClickListener {
 
+    ListView listView;
 
     public HeadlinesFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headlines, container, false);
+        View view = inflater.inflate(R.layout.fragment_headlines, container, false);
+        listView = view.findViewById(R.id.healines_listview);
+        listView.setOnItemClickListener(this);
+        return  view;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        Toast.makeText(getActivity(), (CharSequence) adapterView.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+
+    }
 }
